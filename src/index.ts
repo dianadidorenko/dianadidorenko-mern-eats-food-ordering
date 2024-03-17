11b8a8b3254421c01850a6a22e4da74db2939dd7
Diana Didorenko <125818555+dianadidorenko@users.noreply.github.com>
2024-03-17 14:56:51 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/myUserRoute";
+
+mongoose
+  .connect(process.env.MONGO_URL as string)
+  .then(() => console.log("Connected to DB"));
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK" });
+});
+
+app.use("/api/my/user", myUserRoute);
+
+app.listen(7000, () => {
+  console.log("Server started on localhost:7000");
+});
