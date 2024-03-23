bb61687469bddedc43905a56994dc72dc6ffe796
Diana Didorenko <125818555+dianadidorenko@users.noreply.github.com>
2024-03-24 00:17:08 +0200

    Stripe added

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import myUserRoute from "./routes/MyUserRoute";
 import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 
 mongoose
   .connect(process.env.MONGO_URL as string)
@@ -29,6 +30,7 @@ app.get("/health", async (req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(7000, () => {
   console.log("Server started on localhost:7000");
--- src/models/restaurants.ts
+++ src/models/restaurants.ts
@@ -1,9 +1,18 @@
-import mongoose from "mongoose";
+import mongoose, { InferSchemaType } from "mongoose";
 
 const menuItemsSchema = new mongoose.Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    default: () => new mongoose.Types.ObjectId(),
+  },
   name: { type: String, required: true },
   price: { type: Number, required: true },
 });
+
+// vvv gives us an intellisense info in the code
+
+export type MenuItemType = InferSchemaType<typeof menuItemsSchema>;
 
 const restaurantSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
