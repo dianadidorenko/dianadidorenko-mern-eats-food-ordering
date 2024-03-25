0ace42c58b4f1b783dcaf748f48e05d25c5181c3
Diana Didorenko <125818555+dianadidorenko@users.noreply.github.com>
2024-03-25 16:40:10 +0200

    restaurant order status  v2

--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -9,10 +9,26 @@ const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 5 * 1024 * 1025 }, // 5mb}
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5mb
+  },
 });
 
-// /api/my/restaurant
+router.get(
+  "/order",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.getMyRestaurantOrders
+);
+
+router.patch(
+  "/order/:orderId/status",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateOrderStatus
+);
+
+router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
 
 router.post(
   "/",
@@ -22,7 +38,7 @@ router.post(
   jwtParse,
   MyRestaurantController.createMyRestaurant
 );
-router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
+
 router.put(
   "/",
   upload.single("imageFile"),
